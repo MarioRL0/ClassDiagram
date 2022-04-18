49249d1cf16deb466984b5e33ed7357f2bdd15e5
Mario Romeo Lázaro <91615276+MarioRL0@users.noreply.github.com>
2022-04-18 12:10:38 +0200

    Update Window.java

--- Window.java
+++ Window.java
@@ -4,7 +4,6 @@ import java.awt.event.*;
 
 public class Window implements ActionListener {
 
-	// attributes
 	private Diagram diagram; // draw management
 	private JPanel panel;
 	private GridBagConstraints c;
